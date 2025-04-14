3934d326de8a8f5c27d35c08b0a8b9044a7bf9a6
Callum Lewis <callumlewis852@gmail.com>
2025-04-14 21:29:21 +0100

    trying to fix VS problem

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,10 @@
 package main
-import "fmt"
+import ("fmt"
+    _ "github.com/Masterminds/squirrel"
+    _ "github.com/joho/godotenv"
+    _ "github.com/lann/builder"
+    _ "github.com/lann/ps"
+)
 
 func main() {
 	fmt.Println("Hello World!")
